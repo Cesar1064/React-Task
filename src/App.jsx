d7c9d54d9491fdc9d8ac31b49e6139b95fecb1ff
Cesar1064 <cesar.rz@globant.com>
2024-06-22 19:11:47 -0500

    an error in the routes is corrected

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { FirstTask } from "./pages/FirstTask";
 import { SecondTask } from "./pages/SecondTask";
 import { ThirdTask } from "./pages/ThirdTask";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<NavBar />} />
         <Route path="/FirstTask" element={<FirstTask />} />
         <Route path="/SecondTask" element={<SecondTask />} />
         <Route path="/ThirdTask" element={<ThirdTask />} />
